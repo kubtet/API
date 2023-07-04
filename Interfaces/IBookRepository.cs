343e11cd215eae2aae60650ab6d9b425ff90e4f3
Bartek <bartekmaliniecki@gmail.com>
2023-07-05 01:48:30 +0200

    Removed comment from readbook and added delete book

--- Interfaces/IBookRepository.cs
+++ Interfaces/IBookRepository.cs
@@ -8,13 +8,14 @@ namespace API.Interfaces
     {
         public Task<List<BookDto>> GetAll();
         public Task<BookDetailsDto> GetById(int id);
+        public Task<Boolean> DeleteBook(int id);
         public Task<int> Create(Book book,List<int> genresId);
         public Task<int> AddCover(IFormFile file, int BookId);
         public Task<Boolean> BookExists(string title, string isbn);
         public Task<Boolean> AddGenreToBook(int BookId, int genreId);
         public Task<List<BookDto>> GetBooksByTitle(string title);
         public Task<bool> likeBook(string userName, int BookId);
-        public Task<Boolean> ToRead(string userName, int BookId, int rating, string comment);
+        public Task<Boolean> ToRead(string userName, int BookId, int rating);
         bool BookExists(int id);
         public Task<List<BookDto>> LikedBooks(string userName);
         public Task<List<BookDto>> ReadBooks(string userName);
